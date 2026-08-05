c738a90492f660a869ed1a302b08aa6e37e8ff99
Biplab8 <64832887+Biplab8@users.noreply.github.com>
2026-08-06 01:33:08 +0530

    Bump version code to 2 and version name to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "com.biplab.animan"
 
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
